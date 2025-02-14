9bab055f0635f73c4d59daf121457f49001686f0
HSouch <harrison.souchereau@yale.edu>
2025-02-14 11:49:07 -0500

    Renamed to host_winds for clarity

---
+++ galarp/builtins/host_winds.py
@@ -0,0 +1,12 @@
+from ..components import winds as winds
+
+from astropy import units as u
+
+
+def BasicClusterWind():
+    return winds.ConstantWind(strength=500 * u.km/u.s, disk_wind_angle=0.)
+
+
+def BasicClusterDensity():
+    return winds.ConstantDensity(density=1e-26 * u.g/u.cm**3)
+
