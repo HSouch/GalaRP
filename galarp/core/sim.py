0084d74a5cd75e72aeced9b7c886b71d5f363688
HSouch <harrison.souchereau@yale.edu>
2025-02-17 13:46:28 -0500

    Got general sim running smoothly

--- galarp/core/sim.py
+++ galarp/core/sim.py
@@ -1,36 +1,41 @@
-
+from .outputs import OrbitContainer
 from .. import builtins, components
 
 import numpy as np
 
 from astropy import units as u
 
+from gala import dynamics as gd
 from gala import integrate as gi
 from gala.units import galactic
 
 
 
-def F_RPS(t, particles, potential, wind, rho, shadow, **kwargs):
+def F_RPS(t, w, particles, potential, wind, rho, shadow, kwargs):
     units = kwargs.get("units", galactic)
-    x, y, z, vx, vy, vz = particles.w
-    q = np.stack([x, y, z])
-    p = np.stack([vx, vy, vz])
+    x, y, z, vx, vy, vz = w
+    q = np.stack([x, y, z], axis=1)
+    p = np.stack([vx, vy, vz], axis=1)
     
     _t = np.array([0.0])
-    acc = -potential(q, _t).T
+    acc = -potential._gradient(q, _t).T
     
     # Apply Ram Pressure Wind
     if kwargs.get("wind_on", True):
         v_rel = wind.evaluate(t) - p
-        a_ram = (v_rel ** 2) * (rho.evaluate(t) / particles.sigma_gas)[:, np.newaxis]
+        a_ram = (v_rel ** 2) * (rho.evaluate(t) / particles.surface_density())# [:, np.newaxis]
         a_ram *= np.sign(v_rel)
         a_ram = a_ram.T
     
         if kwargs.get("shadow_on", True) and shadow is not None:
-            damping = shadow.evaluate(t, q, particles.sigms_gas).T
+            damping = shadow.evaluate(t, q, particles.sigma_gas).T
             a_ram *= damping
 
         acc += a_ram
+
+    # Apply shadowing
+    if kwargs.get("shadow_on", True) and shadow is not None:
+        acc *= shadow.evaluate(t, q, particles.sigma_gas)
     
     return np.vstack((p.T, acc))
     
@@ -40,7 +45,7 @@ class RPSim:
                  satellite_potential=builtins.satpots.JZ2023_Satellite(), 
                  particles=components.particles.ExponentialParticleSet(8, 0.5), 
                  wind=builtins.host_winds.BasicClusterWind(), 
-                 rho_icm=(1e-26 * u.g/u.cm**3),
+                 rho_icm=builtins.host_winds.BasicClusterDensity(),
                  shadow=None,
                  method=F_RPS, 
                  units=galactic, 
@@ -54,7 +59,6 @@ class RPSim:
 
         self.method = method
 
-        
         self.units = units
 
         # Make sure we actually have particles to simulate
@@ -68,16 +72,24 @@ class RPSim:
             assert p.units == units_to_compare, "Units do not match"
     
 
-    def run(self, **kwargs):
+    def run(self, dt=5, integration_time=100.,  **kwargs):
         self.ensure_common_units()
 
-        func_args = (self.particles, self.satellite_potential, self.wind, self.rho_icm, self.shadow)
-
+        func_args = (self.particles, self.satellite_potential, self.wind, self.rho_icm, self.shadow, kwargs)
         integrator = gi.RK5Integrator(
             self.method,
             func_args=func_args,
             func_units=galactic,
         )
 
-        return 0
+        self.particles.container.append(self.particles.phase_space_positions())
 
+        orbits = integrator.run(
+            gd.combine(self.particles.container), dt=dt, t1=0, t2=integration_time
+        )
+
+        output_metadata = {}
+
+        return OrbitContainer(data=orbits, 
+                              units=self.units,
+                              metadata=output_metadata)
