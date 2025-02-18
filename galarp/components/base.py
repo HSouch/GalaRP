fa7f2c6dc4dfc15c1e355048e2377bcbbe844ef5
HSouch <harrison.souchereau@yale.edu>
2025-02-18 14:53:02 -0500

    Added unitset to iterator base class

--- galarp/components/base.py
+++ galarp/components/base.py
@@ -1,10 +1,12 @@
 from gala.units import galactic
-
+from ..utils import UnitSet
 
 class Iterator:
     def __init__(self, units=galactic, name=None):
         self.name = name
         self.units = units
+
+        self.unitset = UnitSet(self.units)
 
     def evaluate(self, t):
         raise NotImplementedError("Abstract Base Class")
