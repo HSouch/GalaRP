8b9644bfc2cc1b0732abb0fa5a5189df5a44e82a
HSouch <harrison.souchereau@yale.edu>
2025-02-19 10:47:50 -0500

    Added some built-in particle sets

--- galarp/builtins/__init__.py
+++ galarp/builtins/__init__.py
@@ -1,3 +1,3 @@
 from . import satellite_potentials as satpots
 from . import host_winds as host_winds
-
+from . import particles as particles
--- galarp/builtins/particles.py
+++ galarp/builtins/particles.py
@@ -1,1 +1,11 @@
+from ..components import particles
+from astropy import units as u
+
+def LowResUniformGrid():
+    return particles.UniformGridParticleSet(Rmax=15 * u.kpc, spacing=5 * u.kpc, 
+                                            disp_R=10 * u.km / u.s, disp_z=5 * u.km / u.s)
+
+def HighResUniformGrid():
+    return particles.UniformGridParticleSet(Rmax=15 * u.kpc, spacing=0.2 * u.kpc, 
+                                            disp_R=10 * u.km / u.s, disp_z=5 * u.km / u.s)
 
