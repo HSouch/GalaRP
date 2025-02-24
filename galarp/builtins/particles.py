091d9d49ce8a257409c43e2c27e4bbde024d87a8
Harrison Souchereau <harrison.souchereau@yale.edu>
2025-02-23 23:37:36 -0500

    Changed  builtin parameters

--- galarp/builtins/particles.py
+++ galarp/builtins/particles.py
@@ -2,7 +2,7 @@ from ..components import particles
 from astropy import units as u
 
 def LowResUniformGrid():
-    return particles.UniformGridParticleSet(Rmax=15 * u.kpc, spacing=5 * u.kpc, 
+    return particles.UniformGridParticleSet(Rmax=15 * u.kpc, spacing=0.5 * u.kpc, 
                                             disp_R=10 * u.km / u.s, disp_z=5 * u.km / u.s)
 
 def HighResUniformGrid():
